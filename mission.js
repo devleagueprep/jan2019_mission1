6ad8c52e359dd66e828f189960a7612e4b48b3e6
yung kahuna <tjfaustino7@gmail.com>
2019-01-18 19:36:55 -1000

    troy

--- mission.js
+++ mission.js
@@ -55,10 +55,9 @@ Console.log to see your result.*/
  Create a function named plusOne that takes two parameters arr and str and will add a new string element into an array and will return the array.
  Console.log your result*/
  
-
+ var arr1 = ['one', 'two'];
+ var str1 = 'three';
  function plusOne(arr,str){
-    var arr1 = ['one', 'two'];
-    var str1 = 'three';
     return arr1.push(str1)
  }
 console.log(plusOne(arr1,str1));
