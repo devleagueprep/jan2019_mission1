aae69c60016b5505dccc4a1363d9fc1a3dc91601
unknown <asiabermea@gmail.com>
2019-01-18 19:49:42 -1000

    Exercise 2 Complete

--- mission.js
+++ mission.js
@@ -24,6 +24,10 @@ Console.log to see your result.*/
  Create a function named happyBday that takes two parameters name and age and returns the following: "Happy Birthday name! You look maaahvelous at age!" Where your input will replace the name and age parameters.
  Console log to see your result.*/
  
+ function happyBday(name, age){
+     return ["Happy birthday " + name + "! You look maaahvelous at " + age + "!"]
+ }
+ console.log(happyBday('Felicia', 87))
  
  /*Mission 3: 
  Team Leader: Dom
